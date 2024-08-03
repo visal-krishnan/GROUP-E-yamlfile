91f3703699e6f56a1b70877dd3c17119706e7e91
Bharath-HB <bharathhareesh2002@gmail.com>
2024-08-03 12:00:04 +0530

    push

---
+++ Survey-Service/src/main/java/com/ust/Survery_Service/SurveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ust.Survery_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveryServiceApplication.class, args);
+	}
+
+}

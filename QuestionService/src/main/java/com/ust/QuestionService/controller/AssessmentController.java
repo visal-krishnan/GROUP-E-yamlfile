c06f03628026ce260f1c23ce31586029623ecd20
ADARSH1307 <92903955+ADARSH1307@users.noreply.github.com>
2024-08-07 10:12:10 +0530

    Delete QuestionService directory

--- QuestionService/src/main/java/com/ust/QuestionService/controller/AssessmentController.java
+++
@@ -1,7 +0,0 @@
-package com.ust.QuestionService.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AssessmentController {
-}
--- QuestionService/src/main/java/com/ust/QuestionService/repo/AssessmentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ust.QuestionService.repo;
-
-import com.ust.QuestionService.model.Assessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssessmentRepo extends JpaRepository<Assessment,String> {
-}

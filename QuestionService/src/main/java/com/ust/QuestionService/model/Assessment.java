d887d0d2cb4f067dee6b4f80ddb16c155f329393
visal-krishnan <visalkrishnan01@gmail.com>
2024-08-08 09:36:52 +0530

    updated get mapping

--- QuestionService/src/main/java/com/ust/QuestionService/model/Assessment.java
+++ QuestionService/src/main/java/com/ust/QuestionService/model/Assessment.java
@@ -1,5 +1,6 @@
 package com.ust.QuestionService.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +32,7 @@ public class Assessment {
     @OneToMany
     @JoinColumn(name="setid")
     @Cascade(value= CascadeType.ALL)
+    @JsonIgnore
     private List<Question> questions;
 
 
